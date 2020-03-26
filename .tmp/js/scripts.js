8d2efecb5dc6f3b74a84d58098f210bc29c2fb77
Matvey <lishay.matvey20001506@gmail.com>
2020-03-26 18:46:22 +0300

    toggle - test

--- .tmp/js/scripts.js
+++ .tmp/js/scripts.js
@@ -1,18 +1,8 @@
 "use strict";
 
-var navList = document.querySelector('.header__nav-list');
-var navToggle = document.querySelector('.header__nav-toggle');
-var navToggleBtn = navToggle;
-navToggle.addEventListener('click', function () {
-  if (navList.classList.contains('header__nav-list--closed')) {
-    navList.classList.remove('header__nav-list--closed');
-    navList.classList.add('header__nav-list--opened');
-    navToggleBtn.classList.remove('header__nav-toggle--on');
-    navToggleBtn.classList.add('header__nav-toggle--off');
-  } else {
-    navList.classList.add('header__nav-list--closed');
-    navList.classList.remove('header__nav-list--opened');
-    navToggleBtn.classList.remove('header__nav-toggle--off');
-    navToggleBtn.classList.add('header__nav-toggle--on');
-  }
-});
+function navList() {
+  var navigation = document.getElementById("navList");
+  var btn = document.getElementById('toggle');
+  btn.classList.toggle("js-on-toggle");
+  navigation.classList.toggle("js-on");
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,17 +1,7 @@
-var navList = document.querySelector('.header__nav-list');
-var navToggle = document.querySelector('.header__nav-toggle');
-var navToggleBtn = navToggle;
+function navList() {
+    let navigation = document.getElementById("navList");
+    let btn = document.getElementById('toggle');
+    btn.classList.toggle("js-on-toggle");
+    navigation.classList.toggle("js-on");
 
-navToggle.addEventListener('click', function () {
-    if (navList.classList.contains('header__nav-list--closed')) {
-        navList.classList.remove('header__nav-list--closed');
-        navList.classList.add('header__nav-list--opened');
-        navToggleBtn.classList.remove('header__nav-toggle--on');
-        navToggleBtn.classList.add('header__nav-toggle--off');
-    } else {
-        navList.classList.add('header__nav-list--closed');
-        navList.classList.remove('header__nav-list--opened');
-        navToggleBtn.classList.remove('header__nav-toggle--off');
-        navToggleBtn.classList.add('header__nav-toggle--on');
-    }
-});
+}
