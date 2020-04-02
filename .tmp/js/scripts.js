9571af1cf4fb2e8263cbb521725e107e8363f6fd
matvey <lishay.matvey20001506@gmail.com>
2020-04-02 14:09:46 +0300

    fix cards , added cycle for in twig

--- .tmp/js/scripts.js
+++ .tmp/js/scripts.js
@@ -8,21 +8,23 @@ function navWrap() {
   function toggleNav() {
     btnNav.classList.toggle('is-open');
     navList.classList.toggle('is-on');
+    console.log(btnNav);
   }
 }
 
 navWrap();
 
 function tabWrap() {
-  var tabNav = document.querySelectorAll('.js-tab');
+  var tabNav = Array.prototype.slice.call(document.querySelectorAll('.js-tab'));
   var tabContents = document.querySelectorAll('.js-tab-content');
-  var isActive = document.querySelector('is-active');
+  var activeClass = 'is-active';
   tabNav.forEach(function (trigger) {
     trigger.addEventListener('click', function () {
       var id = this.getAttribute('data-tab');
       var content = document.querySelector('.js-tab-content[data-tab="' + id + '"]');
-      var activeTrigger = document.querySelector('.js-tab.is-active');
-      var activeContent = document.querySelector('.js-tab-content.is-active');
+      var activeTrigger = document.querySelector('.js-tab.' + activeClass);
+      var activeContent = document.querySelector('.js-tab-content.' + activeClass);
+      console.log(tabNav);
       activeTrigger.classList.remove('is-active');
       trigger.classList.add('is-active');
       activeContent.classList.remove('is-active');
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,23 +7,24 @@ function navWrap() {
     function toggleNav() {
         btnNav.classList.toggle('is-open');
         navList.classList.toggle('is-on');
+        console.log(btnNav);
     }
 }
 
 navWrap();
 
 function tabWrap() {
-    const tabNav = document.querySelectorAll('.js-tab');
+    const tabNav = Array.prototype.slice.call(document.querySelectorAll('.js-tab'));
     const tabContents = document.querySelectorAll('.js-tab-content');
-    const isActive = document.querySelector('is-active');
+    const activeClass = 'is-active';
 
     tabNav.forEach(function (trigger) {
         trigger.addEventListener('click', function () {
             const id = this.getAttribute('data-tab');
             const content = document.querySelector('.js-tab-content[data-tab="' + id + '"]');
-            const activeTrigger = document.querySelector('.js-tab.is-active');
-            const activeContent = document.querySelector('.js-tab-content.is-active');
-
+            const activeTrigger = document.querySelector('.js-tab.' + activeClass);
+            const activeContent = document.querySelector('.js-tab-content.' + activeClass);
+            console.log(tabNav);
             activeTrigger.classList.remove('is-active');
             trigger.classList.add('is-active');
 
