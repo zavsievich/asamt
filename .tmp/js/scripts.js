f130da87c1de604853d23f646e873abfa2d60993
matvey <lishay.matvey20001506@gmail.com>
2020-04-02 12:50:38 +0300

    fix classes for js

--- .tmp/js/scripts.js
+++ .tmp/js/scripts.js
@@ -12,52 +12,21 @@ function navWrap() {
 }
 
 navWrap();
-/*
-
-OLD JS CODE  for comparison, delete in a future commit
-
-function tabWrap() {
-    const tabNav = document.querySelectorAll('.js-tab'),
-        tabContent = document.querySelectorAll('.js-tab-content');
-
-    let tabName;
-
-    tabNav.forEach(item => {
-        item.addEventListener('click', selectTabNav)
-    });
-
-    function selectTabNav() {
-        tabNav.forEach(item => {
-            item.classList.remove('is-active');
-        });
-        this.classList.add('is-active');
-        tabName = this.getAttribute('data-tab');
-        selectTabContent(tabName);
-    }
-
-    function selectTabContent(tabName) {
-        tabContent.forEach(item => {
-            item.classList.contains(tabName) ? item.classList.add('is-show') :
-                item.classList.remove('is-show');
-        })
-    }
-}
-
-tabWrap();*/
 
 function tabWrap() {
   var tabNav = document.querySelectorAll('.js-tab');
   var tabContents = document.querySelectorAll('.js-tab-content');
+  var isActive = document.querySelector('is-active');
   tabNav.forEach(function (trigger) {
     trigger.addEventListener('click', function () {
       var id = this.getAttribute('data-tab');
       var content = document.querySelector('.js-tab-content[data-tab="' + id + '"]');
       var activeTrigger = document.querySelector('.js-tab.is-active');
-      var activeContent = document.querySelector('.js-tab-content.is-show');
+      var activeContent = document.querySelector('.js-tab-content.is-active');
       activeTrigger.classList.remove('is-active');
       trigger.classList.add('is-active');
-      activeContent.classList.remove('is-show');
-      content.classList.add('is-show');
+      activeContent.classList.remove('is-active');
+      content.classList.add('is-active');
     });
   });
 }
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -12,55 +12,23 @@ function navWrap() {
 
 navWrap();
 
-/*
-
-OLD JS CODE  for comparison, delete in a future commit
-
-function tabWrap() {
-    const tabNav = document.querySelectorAll('.js-tab'),
-        tabContent = document.querySelectorAll('.js-tab-content');
-
-    let tabName;
-
-    tabNav.forEach(item => {
-        item.addEventListener('click', selectTabNav)
-    });
-
-    function selectTabNav() {
-        tabNav.forEach(item => {
-            item.classList.remove('is-active');
-        });
-        this.classList.add('is-active');
-        tabName = this.getAttribute('data-tab');
-        selectTabContent(tabName);
-    }
-
-    function selectTabContent(tabName) {
-        tabContent.forEach(item => {
-            item.classList.contains(tabName) ? item.classList.add('is-show') :
-                item.classList.remove('is-show');
-        })
-    }
-}
-
-tabWrap();*/
-
 function tabWrap() {
     const tabNav = document.querySelectorAll('.js-tab');
     const tabContents = document.querySelectorAll('.js-tab-content');
+    const isActive = document.querySelector('is-active');
 
     tabNav.forEach(function (trigger) {
         trigger.addEventListener('click', function () {
             const id = this.getAttribute('data-tab');
             const content = document.querySelector('.js-tab-content[data-tab="' + id + '"]');
             const activeTrigger = document.querySelector('.js-tab.is-active');
-            const activeContent = document.querySelector('.js-tab-content.is-show');
+            const activeContent = document.querySelector('.js-tab-content.is-active');
 
             activeTrigger.classList.remove('is-active');
             trigger.classList.add('is-active');
 
-            activeContent.classList.remove('is-show');
-            content.classList.add('is-show');
+            activeContent.classList.remove('is-active');
+            content.classList.add('is-active');
         })
     })
 }
